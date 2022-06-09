c67af9f50667320a4865a18589ee9fa951341791
lay khatri <layk.itpath@gmail.com>
2022-06-09 18:09:21 +0530

    Styling Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [
+    `
+      #attribute {
+        color: blue;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'learning_angular';
