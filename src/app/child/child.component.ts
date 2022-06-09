3a8befe81e22f682331cc3dd59a166a21b144234
lay khatri <layk.itpath@gmail.com>
2022-06-09 11:06:02 +0530

    Child to Parent Communication

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,6 +8,12 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ChildComponent implements OnInit {
   @Input()
   childMessage!: string;
+
+  @Output() parentMessage = new EventEmitter();
+
+  sendMessage() {
+    this.parentMessage.emit('Hello From Child');
+  }
 
   constructor() {}
 
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  receiveMessage(value: string) {
+    alert(value);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
