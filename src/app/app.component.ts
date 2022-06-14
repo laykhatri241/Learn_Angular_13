787e747671d94809a15f03d7265baf07d34d8f8d
lay khatri <layk.itpath@gmail.com>
2022-06-14 11:49:51 +0530

    Structural Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'learning_angular';
+
+  showTitle = true;
+
+  myList = ['hello', 'World', 'Focks'];
+
+  names = { name: 'lay' };
 }
