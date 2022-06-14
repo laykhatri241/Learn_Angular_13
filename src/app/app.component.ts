4c8b56280bc29268dc1401ae21bcffdc6619535a
lay khatri <layk.itpath@gmail.com>
2022-06-14 11:07:06 +0530

    Custom Attribute Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <p appHighlight>title</p> `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'learning_angular';
---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  constructor(er: ElementRef) {
+    er.nativeElement.style.background = 'cyan';
+  }
+}
