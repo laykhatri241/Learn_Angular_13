2ea06904308bb8690be63e5740c0e7aa069dd2a2
lay khatri <layk.itpath@gmail.com>
2022-06-14 10:37:37 +0530

    Built-in Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <p [ngClass]="isSpecial ? 'special' : ''">{{ title }}</p>
+    <br />
+    <h1 [ngStyle]="myStyle">{{ title }}</h1>
+    <br />
+    <input type="text" [(ngModel)]="title" />`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'learning_angular';
+  isSpecial = true;
+  myStyle = {
+    background: 'blue',
+    'text-decoration': 'underline',
+  };
 }
