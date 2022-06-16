875ebcb94898aa6014b58af2418e3c713d2dd439
lay khatri <layk.itpath@gmail.com>
2022-06-16 11:31:22 +0530

    TemplateDiven Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { gaming } from './gaming.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'learning_angular';
+
+  games = ['Dota2', 'CSGO', 'Rainbow Six Siege'];
+
+  model = new gaming(1, 'Lay', this.games[1]);
+
+  isSubmitted = false;
+
+  onSubmit() {
+    this.isSubmitted = true;
+  }
 }
---
+++ src/app/gaming.model.ts
@@ -0,0 +1,3 @@
+export class gaming {
+  constructor(public id: number, public name: string, public game: string) {}
+}
