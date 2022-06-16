791b022d75c4c1af53dbb6ab09ad8ece4b719d47
lay khatri <layk.itpath@gmail.com>
2022-06-16 11:15:47 +0530

    minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FirstComponent } from './first/first.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { SecondComponent } from './second/second.component';
 
 const routes: Routes = [
