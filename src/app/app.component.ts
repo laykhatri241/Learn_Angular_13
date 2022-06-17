65611a6c999f7e2c65740f1a5655a2a71b8ddafc
lay khatri <layk.itpath@gmail.com>
2022-06-17 10:52:16 +0530

    Form Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'learning_angular';
+
+  nameInput = '';
+
+  YourName = new FormGroup({
+    Name: new FormControl(this.nameInput, [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+  });
+
+  get Name() {
+    return this.YourName.get('Name');
+  }
 }
