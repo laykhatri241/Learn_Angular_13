806a20d5a93c0adf177b6d8103ee8117c0e8df46
lay khatri <layk.itpath@gmail.com>
2022-06-17 10:17:56 +0530

    Reactive Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'learning_angular';
+
+  Name = new FormControl('');
+
+  UserCreds = new FormGroup({
+    UserName: new FormControl(''),
+    PassWord: new FormControl(''),
+  });
+
+  updateName() {
+    this.Name.setValue('Lay');
+  }
+
+  onSubmit() {
+    console.log(this.UserCreds.value);
+  }
 }
